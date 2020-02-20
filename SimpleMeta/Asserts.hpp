6797b55d16064ad6e31dc736f4abf05ab0ae8811
jodavis <jodavis42@gmail.com>
2020-02-19 18:26:15 -0800

    Added quick asserts.

---
+++ SimpleMeta/Asserts.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+inline bool SignalError(const char* expression, const char* file, size_t line, bool& ignore, const char* msgFormat, ...)
+{
+  return !ignore;
+}
+
+#define ErrorIf(Expression, ...) \
+  do { static bool __ignore = false; if((Expression) &&SignalError(#Expression, __FILE__, __LINE__, __ignore,##__VA_ARGS__)) \
+  __debugbreak(); } while(false)
